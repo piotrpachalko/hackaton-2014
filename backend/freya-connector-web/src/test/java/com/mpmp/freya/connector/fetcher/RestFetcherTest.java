fec4b3833559e17c9ba1744cff9743919608076f
Michal Przybylak <michal.przybylak@gmail.com>
2014-04-25 23:32:03 +0200

    rest fetcher fetch single object

--- backend/freya-connector-web/src/test/java/com/mpmp/freya/connector/fetcher/RestFetcherTest.java
+++ backend/freya-connector-web/src/test/java/com/mpmp/freya/connector/fetcher/RestFetcherTest.java
@@ -26,7 +26,7 @@ public class RestFetcherTest {
     private static final String HOST = "http://localhost";
 
     private static final String JSON_ARRAY = "{\"results\":[{\"name\" : \"first name\",\"description\" : \"first description\"},{\"name\" : \"second name\",\"description\" : \"second description\"}]}";
-
+    private static final String JSON_OBJECT = "{\"first\":{\"name\" : \"first name\",\"description\" : \"first description\"}}";
     private MockWebServer server;
     
     @Test
@@ -46,24 +46,21 @@ public class RestFetcherTest {
     }
     
     @Test
-    public void shouldRetrieveJsonFromGivenAddress() throws IOException {
+    public void shouldRetrieveJsonArrayFromGivenAddress() throws IOException {
         
         // given
-        server = new MockWebServer();
-        server.play();
-        
-        server.enqueue(new MockResponse().setBody(JSON_ARRAY));
+        setupServerWithResponse(JSON_ARRAY);
         
         Fetcher fetcher = new RestFetcher();
         fetcher.setAddress(server.getUrl("/PATH").toString());
         
         // when
         JSONObject result = fetcher.retrieve();
-        System.out.println(result);
         
         // then
+        server.shutdown();
+
         JSONArray resultArray = (JSONArray) result.get("results");
-        System.out.println(resultArray);
         
         assertThat(resultArray.size()).isEqualTo(2);
         
@@ -74,7 +71,32 @@ public class RestFetcherTest {
         JSONObject secondResult = (JSONObject) resultArray.get(1);
         assertThat(secondResult.get("name")).isEqualTo("second name");
         assertThat(secondResult.get("description")).isEqualTo("second description");
+    }
+
+    private void setupServerWithResponse(String response) throws IOException {
+        server = new MockWebServer();
+        server.play();
+        server.enqueue(new MockResponse().setBody(response));
+    }
+    
+    @Test
+    public void shouldRetriveJsonObjectFromGievenAddress() throws IOException {
         
+        // given
+        setupServerWithResponse(JSON_OBJECT);
+
+        Fetcher fetcher = new RestFetcher();
+        fetcher.setAddress(server.getUrl("/PATH").toString());
+        
+        // when
+        JSONObject result = fetcher.retrieve();
+        JSONObject object = (JSONObject) result.get("first");
+        
+        // then
         server.shutdown();
+        
+        assertThat(result.size()).isEqualTo(1);
+        assertThat(object.get("name")).isEqualTo("first name");
+        assertThat(object.get("description")).isEqualTo("first description");
     }
 }
