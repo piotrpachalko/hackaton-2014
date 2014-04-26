dc28352770583ae52459c655e9f4954fa30d165a
Michal Przybylak <michal.przybylak@gmail.com>
2014-04-26 08:43:54 +0200

    application config

--- backend/freya-connector-web/src/main/java/com/mpmp/freya/connector/ApplicationConfig.java
+++ backend/freya-connector-web/src/main/java/com/mpmp/freya/connector/ApplicationConfig.java
@@ -3,7 +3,7 @@ package com.mpmp.freya.connector;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/")
+@ApplicationPath("/ ")
 public class ApplicationConfig extends Application {
 
 }
